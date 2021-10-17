5d152f92df019fb1a87ef9149a6aab809e4a9418
Diana Aleksieieva <aleksodiana@gmail.com>
2021-10-17 13:18:03 +0300

    corr

--- src/redux/contacts/contacts-type.js
+++
@@ -1,5 +0,0 @@
-
-// const ADDCONTACTS = 'AddContact';
-// const REMOVECONTACTS = 'RemoveContact';
-// const FILTERCONTACTS = 'FilterContact';
-// export default {ADDCONTACTS, REMOVECONTACTS,FILTERCONTACTS }
